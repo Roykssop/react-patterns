cd9defa68a92238267507d3dedcbd8767b0e73af
Roykssop <herna14@gmail.com>
2021-07-11 20:30:35 -0300

    Pattern: Extendible Styles (#3)
    * added normal example
    
    * added example
    
    Co-authored-by: juan-carlos-correa <juan.correa.herrera@gmail.com>

--- src/extensible-styles/components/extensible-styles-page.js
+++ src/extensible-styles/components/extensible-styles-page.js
@@ -1,8 +1,21 @@
 import React from 'react';
 
+import {Button} from './normal-button';
+import {CustomButton} from './custom-button';
+
 export const ExtensibleStylesPage = () => (
   <>
-    <h2>Extensible Styles</h2>
-    <p>Sigue las instrucciones que vienen en el curso.</p>
+    <h2>Ejemplo sin Extensible Styles</h2>
+    <Button type="button">Click</Button>
+    <hr />
+
+    <h2>Ejemplo con Extensible Styles</h2>
+    <CustomButton
+      className="btn btn-success"
+      type="button"
+      style={{color: '#ff0000'}}
+    >
+      Click
+    </CustomButton>
   </>
 );
